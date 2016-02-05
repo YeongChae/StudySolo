eab8b50ec93c416c19695646d89045185a64dac1
YeongChae Kim <neocherry83@gmail.com>
2016-02-05 13:57:44 +0900

    Activity 화면생성과 종료 - 화면 두개를 연결하여 다음화면을 불러오고 닫는 방법을 배워보자 - 안드로이드는 스택(중첩) 방식이다.

--- app/src/main/java/com/example/myapplication/ScrollActivity.java
+++ app/src/main/java/com/example/myapplication/ScrollActivity.java
@@ -1,13 +1,39 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
-public class ScrollActivity extends AppCompatActivity {
+public class ScrollActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mAgeEditText;
+    private EditText mNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 화면에 layout 표시
         setContentView(R.layout.activity_scroll);
+        //이름, 나이
+        mNameEditText = (EditText) findViewById(R.id.name_edit_text);
+        mAgeEditText = (EditText) findViewById(R.id.age_edit_text);
+
+        findViewById(R.id.next_activity_button).setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        // 이름, 나이 가져와서 intent 에 추가
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        intent.putExtra("name", mNameEditText.getText().toString());
+        intent.putExtra("age", mAgeEditText.getText().toString());
+        // secondActivity 로 전환하겠다는 intent
+        // intent 정보를 토대로 다른 Acrivity를 시작
+        startActivity(intent);
+    }
+
 }
