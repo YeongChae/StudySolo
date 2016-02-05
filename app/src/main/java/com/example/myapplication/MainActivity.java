22858b3057de8c614d5ebe8c65d574fc9627801d
YeongChae Kim <neocherry83@gmail.com>
2016-02-05 09:44:36 +0900

    Button Click Event 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,10 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
@@ -17,21 +15,27 @@ public class MainActivity extends AppCompatActivity {
         //화면에 layout 표시
         setContentView(R.layout.button_exam);
         // View에 있는 인터페이스를 사용하여 메세지를 띄우는 방식.
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "클릭매우잘됨", Toast.LENGTH_SHORT).show();
-            }
-        };
+
         //두번째 버튼 id를 지정해주면 끌고 올 수 있다.
-        Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(listener);
+//        Button button2 = (Button) findViewById(R.id.button2);
+//        button2.setOnClickListener(this); //익명클래스
+        findViewById(R.id.button2).setOnClickListener(this);
+//        Button button3 = (Button) findViewById(R.id.button3);
+//        button3.setOnClickListener(this); //익명클래스
+        findViewById(R.id.button3).setOnClickListener(this);
+
     }
 
-    // activity_main.xml 에 연결 됨.
+    @Override
     public void onClick(View view) {
-        Log.d(TAG, "클릭 잘 됨");
-        // 경고메세지같은거 띄울때 활용하기 좋은 Toast
-        Toast.makeText(MainActivity.this, "클릭잘됨", Toast.LENGTH_SHORT).show();
+        switch (view.getId()) {
+            case R.id.button2:
+                Toast.makeText(MainActivity.this, "버튼2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(MainActivity.this, "버튼3", Toast.LENGTH_SHORT).show();
+                break;
+        }
+//        Toast.makeText(MainActivity.this, "클릭매우잘됨", Toast.LENGTH_SHORT).show();
     }
 }
