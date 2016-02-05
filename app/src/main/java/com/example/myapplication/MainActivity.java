d7b2d595df39238f2a09eadea416bf8d51189efd
YeongChae Kim <neocherry83@gmail.com>
2016-02-05 10:03:19 +0900

    CheckBox 1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,40 +2,39 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //화면에 layout 표시
-        setContentView(R.layout.button_exam);
-        // View에 있는 인터페이스를 사용하여 메세지를 띄우는 방식.
+        setContentView(R.layout.checkbox_exam);
 
-        //두번째 버튼 id를 지정해주면 끌고 올 수 있다.
-//        Button button2 = (Button) findViewById(R.id.button2);
-//        button2.setOnClickListener(this); //익명클래스
-        findViewById(R.id.button2).setOnClickListener(this);
-//        Button button3 = (Button) findViewById(R.id.button3);
-//        button3.setOnClickListener(this); //익명클래스
-        findViewById(R.id.button3).setOnClickListener(this);
+        mTextView = (TextView) findViewById(R.id.msg_text_view);
+        ((CheckBox) findViewById(R.id.checkbox)).setOnCheckedChangeListener(this);
+
 
     }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button2:
-                Toast.makeText(MainActivity.this, "버튼2", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button3:
-                Toast.makeText(MainActivity.this, "버튼3", Toast.LENGTH_SHORT).show();
-                break;
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        Toast.makeText(MainActivity.this, "check : " + isChecked, Toast.LENGTH_SHORT).show();
+
+        // TextView 글자를 변형
+        if (isChecked) {
+            mTextView.setText("체크됨");
+        } else {
+            mTextView.setText(" ");
         }
-//        Toast.makeText(MainActivity.this, "클릭매우잘됨", Toast.LENGTH_SHORT).show();
     }
+
 }
